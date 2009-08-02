3a6e160bf1e2378b117fea3db05edf43294de445
huseyin.asliturk <huseyin.asliturk@localhost>
2009-08-02 17:26:57 +0300

    pdfmod

---
+++ src/pdfmod/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 Hüseyin ASLITÜRK (huseyin@asliturk.com)
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.export("MONO_SHARED_DIR", get.workDIR())
+    autotools.configure()
+
+def build():
+    shelltools.export("MONO_SHARED_DIR", get.workDIR())
+    autotools.make()
+
+def install():
+    shelltools.export("MONO_SHARED_DIR", get.workDIR())
+
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    autotools.install()
